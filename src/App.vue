73836d85d259bae0a011c675177b45cca54ddc72
CLG\dmoore <devante.moore@cambiumassessment.com>
2021-12-12 00:09:08 -0600

    deleted boiler plate code

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <template>
+<div>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
   </div>
-  <router-view />
+  <router-view></router-view>
+</div>
 </template>
+<script lang='ts' setup>
+</script>
+<script lang='ts'>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+</script>
+<style lang='scss'>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <div class="home">Home</div>
 </template>
 
+<script lang="ts" setup></script>
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+import { defineComponent } from "vue";
+export default defineComponent({});
 </script>
+<style></style>
